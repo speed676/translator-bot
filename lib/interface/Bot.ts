e69360214c036115914d4856118c6b9eec611dde
Victor Guillem Agustí <vidasoft.app@gmail.com>
2017-07-28 14:17:21 +0200

    -Eliminado TypeScript (me da por saco que no veas, y además en el futuro puede ser un problema ya que solo se usa aquí) -Añadido validador básico (hay que añadirle casos) -Añadido test para kik

--- lib/interface/Bot.ts
+++
@@ -1,6 +0,0 @@
-export interface Bot{
-
-	translateToSystem(target:JSON);
-	translateToBot(target:JSON);
-
-}
--- test/kik-test.js
+++ test/kik-test.js
@@ -1,14 +1,64 @@
 let assert = require('assert');
-
+let kik = require("../lib/KikBot");
+let validador = require("../lib/TJSONValidator");
 
 describe('TEST', function() {
   	describe('All platforms', function() {
-    	it('kik example', function() {
-    		
-    		let jsonKik = {'hola': 'mundo'};
 
-    		assert.equal(jsonKik, kik.translateToSystem());
-      		// assert.equal(-1, [1,2,3].indexOf(4));
-    	});
+      let jsonKik = {
+        "chatId": "001",
+        "type": "text",
+        "to": "001",
+        "text": "hola mundo",
+        "buttons": [
+          {
+            "type": "default",
+            "title": "boton1",
+            "value": "1"
+          },
+          {
+            "type": "default",
+            "title": "boton2",
+            "value": "2"
+          }
+        ]
+      };
+
+      let jsonGenerico = { 
+          "idReceptor": "001", 
+          "idCanal": "001", 
+          "mensaje": { 
+            "text": "hola mundo", 
+            "type": "text"
+          },
+          "buttons": [{
+            "title": "boton1"
+          },
+          {
+            "title": "boton2"
+          }]
+        };
+
+      // let traducidoABot = kik.translateToSystem(jsonGenerico);
+
+      // let traducidoASystem = kik.translateToBot(jsonKik);
+
+      it('validar objeto JSONKIK:', function(){
+
+        assert.equal(true, validador.validate(jsonKik).success);
+      });
+      it('validar objeto jsonGenerico:', function(){
+        assert.equal(true, validador.validate(jsonGenerico).success);
+        
+      });
+
+      // it('Traducir a lenguage de KIK:', function(){
+      //   assert.deepEqual(jsonKik, traducidoABot);
+      // });
+
+      // it('Traducir de KIK a lenguaje generico:', function(){
+      //   assert.deepEqual(jsonKik, traducidoABot);
+      // });
+
     });
 });
